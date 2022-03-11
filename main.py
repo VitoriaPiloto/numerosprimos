f24721608c5faa5e8529e56087f78ee0cf3e2b50
Vitória Piloto <57605180+VitoriaPiloto@users.noreply.github.com>
2022-03-11 16:50:47 -0300

    try e except

--- main.py
+++ main.py
@@ -6,27 +6,34 @@ while continua in ('S','s'):
   limite = int(limite)
   primos=[]
   i=base
-  
-  if (limite>100000) or (base>limite) or (base<10):
-    print('Base', base, 'e limite', limite, ': ERRO' )
-  else:
-    if (base==limite): 
-      print('Base %d e Limite %d: VAZIO'%(base,limite))
+  try:
+    if (limite>100000) or (base>limite) or (base<10):
+      print('Base', base, 'e limite', limite, ': ERRO' )
+      continua='n'
     else:
-      print('Base',base,'e Limite',limite,': ', end="")
-      for i in range(base, limite+1): 
-              if i>10: 
-                for j in range(2,i): 
-                    if(i % j==0): 
-                        break
-                else: 
-                    primos.append(i) 
-      for primos in primos : 
-        primos_str = str(primos)
-        print(primos_str, end=", ")
-        i += 1;
-      #except: 
-            #print('Base %d e Limite %d: ERRO'%(base,limite))
-      
-  continua=input('\nPara nova execução (S,s): ')
-
+      if (base==limite): 
+        print('Base %d e Limite %d: VAZIO'%(base,limite))
+        continua='n'
+      else: 
+        print('Base',base,'e Limite',limite,': ', end="")
+        for i in range(base, limite+1):  
+            for j in range(2,i): 
+              if(i % j==0): 
+                break
+            else: 
+              primos.append(i)
+                
+        for primos in primos : 
+          primos_str = str(primos)
+          print(primos_str, end=", ")
+          i += 1
+    
+        continua=input('\nPara nova execução (S,s): ')
+        
+  except ValueError: 
+    print('Base %d e Limite %d: ERRO'%(base,limite))
+    continua='n'
+  
+  except TypeError:
+    print('Base %d e Limite %d: ERRO'%(base,limite))
+    continua='n'
